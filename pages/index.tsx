4d8e5aacd8b1a575787b28baab84107c55d2eacb
Alex L <lexa.act@gmail.com>
2019-01-02 13:41:43 +0100

    chore: add some packages, remove some - update tsconfig - update index page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,41 @@ import * as React from 'react'
 import Link from 'next/link'
 import { style } from 'typestyle'
 
+import { compose } from '@typed/compose'
+
+type Item = {
+  label: string
+  id: number
+}
+
+const items: Item[] = [
+  { label: 'one', id: 1 },
+  { label: 'two', id: 2 },
+  { label: 'three', id: 3 },
+  { label: 'four', id: 4 }
+]
+
+function labelToUpperCase(item: Item, key?: number): React.ReactElement<{}> {
+  return <p key={key}>{item.label.toUpperCase()}</p>
+}
+
+function labelsToUpperCase(_items: Item[]): React.ReactElement<{}>[] {
+  return _items.map(labelToUpperCase)
+}
+
+function filterEvenItem(item: Item): boolean {
+  return item.id % 2 === 0
+}
+
+function filterEvenItems(_items: Item[]): Item[] {
+  return _items.filter(filterEvenItem)
+}
+
+const evenLabelsToUpperCase = compose<Item[], Item[], React.ReactElement<{}>[]>(
+  labelsToUpperCase,
+  filterEvenItems
+)
+
 const textStyles = style({
   fontSize: '40px'
 })
@@ -12,6 +47,7 @@ export const Home = () => (
       <a className={style({ display: 'block', marginBottom: '1rem' })}>other ➡️</a>
     </Link>
     <p className={textStyles}>content</p>
+    {evenLabelsToUpperCase(items)}
   </div>
 )
 
