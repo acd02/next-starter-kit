355038c567144b91a6cd14e48b39150e103eb9ac
acd02 <alex.acd02@gmail.com>
2019-07-10 21:57:37 +0200

    chore: update next to v9

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 const computePath = require('./utils').computePath
 
-/* eslint-disable */
-const withTypescript = require('@zeit/next-typescript')
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-/* eslint-enable */
-
-module.exports = withTypescript({
+module.exports = {
   webpack(config, options) {
     const esLintRule = {
       test: /\.tsx$/,
@@ -14,8 +9,6 @@ module.exports = withTypescript({
       loader: 'eslint-loader'
     }
     config.module.rules.push(esLintRule)
-    // Do not run type checking twice:
-    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
 
     config.resolve.alias = Object.assign({}, config.resolve.alias, {
       components: computePath('./components/'),
@@ -26,4 +19,4 @@ module.exports = withTypescript({
 
     return config
   }
-})
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react'
 import App, { Container } from 'next/app'
 
+/**
+ * HACK
+ *
+ * 1. workaround until the @types/next are fixed
+ */
+type Props = {
+  props: {
+    Component: React.Factory<{}> /* 1 */
+  }
+}
+
 class Root extends App {
   public render() {
-    const { Component } = this.props
+    const {
+      props: { Component }
+    } = (this as unknown) as Props /* 1 */
 
     return (
       <Container>
