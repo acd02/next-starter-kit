e69524e597b3f982890ddd04da9f1012f0ee25fd
acd02 <alex.acd02@gmail.com>
2019-09-01 16:33:48 +0200

    refactor: clean up code

--- pages/other.tsx
+++ pages/other.tsx
@@ -5,28 +5,24 @@ import * as React from 'react'
 export default function OtherPage() {
   return (
     <MainLayout title="other">
-      <Switch />
+      <Content />
     </MainLayout>
   )
 }
 
-function Switch() {
+const btnStyles = css({
+  backgroundColor: '#eee',
+  marginBottom: '1rem',
+  padding: '0.5rem 1rem',
+  cursor: 'pointer'
+})
+
+function Content() {
   const [isOpen, setIsOpen] = React.useState(false)
-
-  function handleClick() {
-    setIsOpen(s => !s)
-  }
-
-  const btnStyles = css({
-    backgroundColor: '#eee',
-    marginBottom: '1rem',
-    padding: '0.5rem 1rem',
-    cursor: 'pointer'
-  })
 
   return (
     <>
-      <button css={btnStyles} onClick={handleClick}>
+      <button css={btnStyles} onClick={() => setIsOpen(s => !s)}>
         update state
       </button>
       <div>
