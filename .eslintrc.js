bd9a51e0a2a8ab1e13ee27ac70fa8e2d3ad5cae0
acd02 <alex.acd02@gmail.com>
2019-09-01 16:00:41 +0200

    chore: update packages - add eslint plugins - update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  plugins: ['@typescript-eslint', 'react-hooks'],
+  plugins: ['@typescript-eslint', 'react-hooks', 'fp', 'simple-import-sort'],
   extends: [
     'standard',
+    'plugin:fp/recommended',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:react/recommended',
@@ -24,15 +25,27 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react/jsx-key': 0,
     'react/display-name': [0, { ignoreTranspilerName: false }],
+    // fp
+    'fp/no-nil': 'off',
+    'fp/no-unused-expression': 'off',
+    'fp/no-rest-parameters': 'off',
+    // import
+    'import/no-absolute-path': 0,
+    // simple-import-sort
+    'simple-import-sort/sort': 1,
     // Misc
-    'max-lines': [1, { max: 100, skipBlankLines: true, skipComments: true }],
+    complexity: [1, 5],
+    curly: ['error', 'multi', 'consistent'],
+    'max-lines': [1, { max: 180, skipBlankLines: true, skipComments: true }],
+    'max-lines-per-function': [1, { max: 50, skipComments: true }],
     'max-len': ['error', { code: 90, ignoreTemplateLiterals: true }],
+    'max-depth': [2, 2],
+    'max-nested-callbacks': [1, 2],
     'newline-before-return': 2,
-    'no-console': ["error", { allow: ["error"] }],
+    'no-console': ['error', { allow: ['error'] }],
     'no-debugger': 2,
     'no-shadow': 2,
     'no-var': 2,
-    'prefer-const': 2,
     quotes: ['error', 'single', { allowTemplateLiterals: false }],
     'space-before-function-paren': ['error', 'never']
   }
