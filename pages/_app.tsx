10f2993ee49c73dcd1afd15dff7059442607bbe3
acd02 <alex.acd02@gmail.com>
2019-12-20 21:10:33 +0100

    refactor: clean up code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,12 @@
 import App from 'next/app'
 import * as React from 'react'
 
-/**
- * HACK
- *
- * 1. workaround until the @types/next are fixed
- */
-type Props = {
-  props: {
-    Component: React.Factory<{}> /* 1 */
-  }
-}
-
 /* eslint-disable fp/no-class, fp/no-this */
 class Root extends App {
   public render() {
     const {
       props: { Component }
-    } = (this as unknown) as Props /* 1 */
+    } = this as any
 
     return <Component />
   }
