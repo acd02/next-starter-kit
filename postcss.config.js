3b919cd8e586e02c011f27181b2e46b1e485d3d7
acd02 <alex.acd02@gmail.com>
2020-02-29 12:31:12 +0100

    chore: update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,18 @@
 const purgecss = [
   '@fullhuman/postcss-purgecss',
   {
-    content: ['./components/**/*.tsx', './pages/**/*.tsx'],
+    content: [
+      './src/components/**/*.tsx',
+      './src/pages/**/*.tsx',
+      './src/pagesContent/**/*.tsx'
+    ],
     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
   }
 ]
 
 module.exports = {
   plugins: [
+    'postcss-nesting',
     'postcss-import',
     'tailwindcss',
     'autoprefixer',
