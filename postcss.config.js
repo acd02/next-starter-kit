c4c2a68a5e1457763d785c8b11b2e978f1630294
acd02 <alex.acd02@gmail.com>
2020-02-29 14:45:20 +0100

    feat: add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -16,6 +16,7 @@ module.exports = {
     'postcss-import',
     'tailwindcss',
     'autoprefixer',
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    ...(process.env.NODE_ENV === 'production' ? ['cssnano'] : []),
   ]
 }
