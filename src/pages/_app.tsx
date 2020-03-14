309c8af04c7aca414e29476eb7968cfb1b82168b
acd02 <alex.acd02@gmail.com>
2020-03-14 10:43:07 +0100

    feat: update next to "9.3.0", update some other packages as well

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,18 @@
 import '../css/tailwind.css'
 
-import App from 'next/app'
+import { AppProps } from 'next/app'
 import Router from 'next/router'
 import * as NProgress from 'nprogress'
 import * as React from 'react'
 
-/* eslint-disable @typescript-eslint/no-explicit-any, fp/no-class, fp/no-this */
 Router.events.on('routeChangeStart', () => {
   NProgress.start()
 })
 Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
-class Root extends App {
-  public render() {
-    const {
-      props: { Component, pageProps }
-    } = this as any
-
-    return <Component {...pageProps} />
-  }
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
-export default Root
+export default App
