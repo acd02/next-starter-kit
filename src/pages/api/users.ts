31e179c432d92405606ca69695406462404bb4ee
acd02 <alex.acd02@gmail.com>
2020-03-14 19:02:03 +0100

    refactor: remove unused code

--- src/pages/api/users.ts
+++
@@ -1,9 +0,0 @@
-import { User } from 'models/user'
-import { NextApiRequest, NextApiResponse } from 'next'
-import { get } from 'utils/http'
-
-export default async (_req: NextApiRequest, res: NextApiResponse) => {
-  return (
-    await get<User[], { status: number }>('https://jsonplaceholder.typicode.com/users')
-  ).fold(e => res.status(e.status).json(e), res.json)
-}
