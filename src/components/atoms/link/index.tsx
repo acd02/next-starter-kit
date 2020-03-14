5b999c8a78d0f3b71642e16e679150f56dbdba8d
acd02 <alex.acd02@gmail.com>
2020-03-14 10:50:43 +0100

    fix: preventDefault on "Link" component

--- src/components/atoms/link/index.tsx
+++ src/components/atoms/link/index.tsx
@@ -57,6 +57,8 @@ export function DynamicLink(props: DynamicLinkProps) {
 }
 
 function handleKeyPress(e: React.KeyboardEvent<HTMLAnchorElement>) {
+  e.preventDefault()
+
   if (e.key === 'Enter' || e.location === 0) {
     const target = e.target as HTMLAnchorElement
     target.click()
