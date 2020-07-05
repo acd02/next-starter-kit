7a92f549e54fe26dfe1dba988670371c07ab6d23
acd02 <alex.acd02@gmail.com>
2020-07-05 18:01:12 +0200

    fix: add Head to custom document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 import * as React from 'react'
 
 /* eslint-disable-next-line fp/no-class */
@@ -6,6 +6,7 @@ export default class MyDocument extends Document {
   public render() {
     return (
       <Html {...{ lang: 'eng' }}>
+        <Head />
         <body>
           <Main />
           <NextScript />
