821367bcf2d8796e30facbe799e0e7683bcee3ad
acd02 <alex.acd02@gmail.com>
2021-05-22 13:28:20 +0200

    refactor: clean up code

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -13,23 +13,23 @@ const styles = css`
 `
 
 const Error: NextPage<Props, unknown> = ({ statusCode }) => {
-  const getContent = () => {
+  const content = (() => {
     if (statusCode === 404) return 'Oops, missing page'
     if (statusCode) return `An error ${statusCode} occurred on the server`
     else return 'An error occurred on the client'
-  }
+  })()
 
-  return <div css={styles}>{getContent()}</div>
+  return <div css={styles}>{content}</div>
 }
 
 Error.getInitialProps = ({ err, res }): Props => {
-  const getStatusCode = () => {
+  const statusCode = (() => {
     if (err) return err.statusCode
     if (res) return res.statusCode
     else return 404
-  }
+  })()
 
-  return { statusCode: getStatusCode() }
+  return { statusCode }
 }
 
 export default Error
