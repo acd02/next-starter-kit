8e5d8b0256e87d8bf0111ff3466f6077d3649da7
acd02 <alex.acd02@gmail.com>
2021-06-20 19:37:53 +0200

    chore: remove unused packages

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins:
-    process.env.NODE_ENV === 'production'
-      ? ['postcss-nesting', 'postcss-import', 'autoprefixer']
-      : ['postcss-nesting', 'postcss-import'],
-}
