0985edaa82d7197cd23f4a80e85dae1bfc98c26c
acd02 <alex.acd02@gmail.com>
2021-10-30 11:02:21 +0200

    refactor: convert "_document" to function

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,15 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 import { getCssText } from 'stitches'
 
-/* eslint-disable-next-line fp/no-class */
-export default class MyDocument extends Document {
-  public render() {
-    return (
-      <Html {...{ lang: 'en' }}>
-        <Head />
-        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html {...{ lang: 'en' }}>
+      <Head />
+      <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
